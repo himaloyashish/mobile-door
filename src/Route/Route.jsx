6ee195aa320fe72701d778a9de0325d26c61a6a5
himaloyash <himaloyashis98@gmail.com>
2023-12-20 13:28:39 +0600

    mobile door is running now on port 5175

--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -1,5 +1,6 @@
 import {createBrowserRouter} from "react-router-dom";
 import Main from "../layout/Main";
+import Home from "../Page/Home/Home";
 
 const router = createBrowserRouter([
     {
@@ -8,7 +9,7 @@ const router = createBrowserRouter([
         children:[
             {
                 path:'/',
-                element:''
+                element:<Home></Home>
             }
         ]
     }
